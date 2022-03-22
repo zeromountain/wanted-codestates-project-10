b61914bd29cc60a897b89d1278aaa2793fb99d73
zeromountain <sonyeongsan@gmail.com>
2022-03-22 23:26:21 +0900

    [#11]feat: implement subsearch component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import {useActivate} from '../hooks/useActivate';
+
 import News from './News';
 import Search from './Search';
+import SubSearch from './SubSearch';
 
 const Main = () => {
+  const searchRef = useRef(null);
+  const [isSearchActive, setIsSearchActive] = useActivate(searchRef, false);
+
+  const searchActive = () => setIsSearchActive(!isSearchActive);
+
   return (
-    <main className="w-full min-h-main flex flex-col justify-between bg-[#CAE9FF] pt-20 tablet:pb-5">
-      <Search />
-      <News />
-    </main>
+    <>
+      <main className="w-full min-h-main flex flex-col justify-between bg-[#CAE9FF] pt-20 tablet:pb-5">
+        <Search searchActive={searchActive} />
+        <News />
+      </main>
+      {isSearchActive && (
+        <SubSearch ref={searchRef} searchActive={searchActive} />
+      )}
+    </>
   );
 };
 
