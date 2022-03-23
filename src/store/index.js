968d4b5a092306cea7db1d707359005f749f5cdc
zeromountain <sonyeongsan@gmail.com>
2022-03-23 22:29:11 +0900

    [#17]-implement search func

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {setupListeners} from '@reduxjs/toolkit/dist/query';
+
+import {searchApi} from '../services/search';
+import formSlice from './formSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [searchApi.reducerPath]: searchApi.reducer,
+    form: formSlice,
+  },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({serializableCheck: false}),
+    getDefaultMiddleware({serializableCheck: false}).concat(
+      searchApi.middleware,
+    ),
 });
+
+setupListeners(store.dispatch);
